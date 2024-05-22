301f0fffd2e687b8959d8e8c76fdd94e4f775503
jasmeendesai <jasmeendesai2597@gmail.com>
2024-05-22 23:49:50 +0530

    added loader

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,47 +1,64 @@
-"use client"
+"use client";
 import { useState } from "react";
 import style from "./contact.module.css";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import CallIcon from '@mui/icons-material/Call';
-import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import CallIcon from "@mui/icons-material/Call";
+import RoomOutlinedIcon from "@mui/icons-material/RoomOutlined";
 import Header from "@/components/header/Header";
 
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 function ContactPage() {
+  const [sentMessage, setSentMessage] = useState(null)
 
-  const [inputs, setInputs] = useState({
-    email: "",
-    name : "",
-    message : ""
-  })
+  const form = useRef();
 
-  const handleChange = e =>{
-    setInputs(prev=> ({...prev, [e.target.name] : e.target.value}))
-  }
+  // console.log(form.current)
 
-  const handleClick = e =>{
-    e.preventDefault()
-    console.log(inputs)
-  }
-
-
+  const sendEmail = (e) => {
+    e.preventDefault();
+    // console.log(e.current.name)
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_YOUR_SERVICE_ID, 
+        process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID, 
+        form.current,
+        {
+        publicKey: process.env.NEXT_PUBLIC_YOUR_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+         
+          console.log("SUCCESS!");
+          setSentMessage("Message Sent!")
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setSentMessage(error.text)
+        }
+      );
+  };
 
   return (
     <div className={style.container}>
       <div className={style.contact}>
-        <Header title={"CONTACT"}/>
+        <Header title={"CONTACT"} />
         <div className={style.contact_details}>
           <div className={style.leftBox}>
-            <form>
+
+            <form ref={form} onSubmit={sendEmail}>
               <label>Name</label>
-              <input type="text" value={inputs.name} name="name"  onChange={handleChange} />
+              <input type="text" name="user_name" />
               <label>Email</label>
-              <input type="text" value={inputs.email} name="email" onChange={handleChange} />
+              <input type="email" name="user_email" />
               <label>Message</label>
-              <textarea type="text" style={{height : "150px"}} value={inputs.message} name="message" onChange={handleChange} rows={10} cols={40}/>
-              <button onClick={handleClick}>Send Message</button>
+              <textarea name="message" rows={8} cols={40} />
+              <button type="submit">Send Message</button>
             </form>
+            {sentMessage && sentMessage}
           </div>
           <div className={style.rightBox}>
             <div className={style.rightBoxTop}>
@@ -50,24 +67,27 @@ function ContactPage() {
                 <p>jasmeendesai2597@gmail.com</p>
               </div>
               <div className={style.item}>
-                <CallIcon className={style.icon1} fontSize="small"/>
+                <CallIcon className={style.icon1} fontSize="small" />
                 <p>+91 8546804058</p>
               </div>
               <div className={style.item}>
-                <RoomOutlinedIcon className={style.icon1} fontSize="small"/>
+                <RoomOutlinedIcon className={style.icon1} fontSize="small" />
                 <p>Belagavi, Karnataka</p>
               </div>
             </div>
 
             <div className={style.rightBoxBottom}>
-            <div className={style.icon}>
-            <a href="https://linkedin.com/in/jasmeen-desai-4b1770275"><LinkedInIcon fontSize="medium"/></a>           
-          </div>
-          <div className={style.icon}>
-            <a href="https://github.com/jasmeendesai"><GitHubIcon fontSize="medium"/></a>
-          </div>
+              <div className={style.icon}>
+                <a href="https://linkedin.com/in/jasmeen-desai-4b1770275">
+                  <LinkedInIcon fontSize="medium" />
+                </a>
+              </div>
+              <div className={style.icon}>
+                <a href="https://github.com/jasmeendesai">
+                  <GitHubIcon fontSize="medium" />
+                </a>
+              </div>
             </div>
-              
           </div>
         </div>
       </div>
@@ -76,3 +96,6 @@ function ContactPage() {
 }
 
 export default ContactPage;
+
+
+
